38a2870005768f098615ec307f90c741021e1bf6
RobertD502 <unlisted@gmail.com>
2022-10-07 17:04:50 -0400

    enumerate constants

--- cowayaio/constants.py
+++ cowayaio/constants.py
@@ -1,17 +1,33 @@
-BASE_URI = 'https://iocareapp.coway.com/bizmob.iocare'
-OAUTH_URL = "https://idp.coway.com/oauth2/v1/authorize"
-SIGNIN_URL = "https://idp.coway.com/user/signin/"
-REDIRECT_URL = "https://iocareapp.coway.com/bizmob.iocare/redirect/redirect.html"
-CLIENT_ID = "UmVuZXdhbCBBcHA"
-SERVICE_CODE = "com.coway.IOCareKor"
-USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1"
+"""Constants for CowayAIO"""
 
-DEVICE_LIST = 'CWIG0304'
-TOKEN_REFRESH = 'CWIL0100'
-STATUS = 'CWIG0602'
-CONTROL = 'CWIG0603'
-FILTERS = 'CWIA0120'
-MCU_VERSION = "CWIG0615"
+from .str_enum import StrEnum
+
+class Endpoint(StrEnum):
+
+    BASE_URI = 'https://iocareapp.coway.com/bizmob.iocare'
+    OAUTH_URL = "https://idp.coway.com/oauth2/v1/authorize"
+    SIGNIN_URL = "https://idp.coway.com/user/signin/"
+    REDIRECT_URL = "https://iocareapp.coway.com/bizmob.iocare/redirect/redirect.html"
+
+
+class Parameter(StrEnum):
+
+    CLIENT_ID = "UmVuZXdhbCBBcHA"
+    SERVICE_CODE = "com.coway.IOCareKor"
+
+class Header(StrEnum):
+
+    USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1"
+
+
+class Endpoint_JSON(StrEnum):
+
+    DEVICE_LIST = 'CWIG0304'
+    TOKEN_REFRESH = 'CWIL0100'
+    STATUS = 'CWIG0602'
+    CONTROL = 'CWIG0603'
+    FILTERS = 'CWIA0120'
+    MCU_VERSION = "CWIG0615"
 
 TIMEOUT = 5 * 60
 
