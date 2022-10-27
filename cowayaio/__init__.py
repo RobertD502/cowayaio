6da9a508c6ebf449f978e9b21c51d7228e4c83e1
Robert Drinovac <52541649+RobertD502@users.noreply.github.com>
2022-10-26 21:19:38 -0400

    remove unused imports

--- cowayaio/__init__.py
+++ cowayaio/__init__.py
@@ -1,11 +1,11 @@
 """Init file for CowayAIO"""
 
-from .constants import (BS, Endpoint, Endpoint_JSON, Header, pad, Parameter, TIMEOUT,)
+from .constants import (Endpoint, Endpoint_JSON, Header, Parameter, TIMEOUT,)
 from .coway_client import (CowayClient, LOGGER,)
 from .exceptions import (AuthError, CowayError,)
 from .purifier_model import (CowayPurifier, PurifierData,)
 from .str_enum import StrEnum
 
-__all__ = ['AuthError', 'BS', 'CowayClient', 'CowayError', 'CowayPurifier',
-           'Endpoint', 'Endpoint_JSON', 'Header', 'LOGGER', 'pad', 'Parameter',
+__all__ = ['AuthError', 'CowayClient', 'CowayError', 'CowayPurifier',
+           'Endpoint', 'Endpoint_JSON', 'Header', 'LOGGER', 'Parameter',
            'PurifierData', 'TIMEOUT', 'StrEnum']
