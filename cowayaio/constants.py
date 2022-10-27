05fd749d9de0d09d7d569011d2f9e8e04d9d00a5
Robert Drinovac <52541649+RobertD502@users.noreply.github.com>
2022-10-26 21:21:07 -0400

    add new openid endpoints

--- cowayaio/constants.py
+++ cowayaio/constants.py
@@ -5,19 +5,21 @@ from .str_enum import StrEnum
 class Endpoint(StrEnum):
 
     BASE_URI = 'https://iocareapp.coway.com/bizmob.iocare'
-    OAUTH_URL = "https://idp.coway.com/oauth2/v1/authorize"
-    SIGNIN_URL = "https://idp.coway.com/user/signin/"
-    REDIRECT_URL = "https://iocareapp.coway.com/bizmob.iocare/redirect/redirect.html"
+    OAUTH_URL = "https://id.coway.com/auth/realms/cw-account/protocol/openid-connect/auth"
+    REDIRECT_URL = "https://iocareapp.coway.com/bizmob.iocare/redirect/redirect_bridge.html"
 
 
 class Parameter(StrEnum):
 
-    CLIENT_ID = "UmVuZXdhbCBBcHA"
+    CLIENT_ID = "cwid-prd-iocare-20220930"
     SERVICE_CODE = "com.coway.IOCareKor"
+    APP_VERSION = "2.3.22"
 
 class Header(StrEnum):
 
-    USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1"
+    USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1 app"
+    ACCEPT = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+    ACCEPT_LANG = "en-US,en;q=0.9"
 
 
 class Endpoint_JSON(StrEnum):
@@ -30,6 +32,3 @@ class Endpoint_JSON(StrEnum):
     MCU_VERSION = "CWIG0615"
 
 TIMEOUT = 5 * 60
-
-BS = 16
-pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
