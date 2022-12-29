ba8111526f4223c1061d990f384c3a58bdd438ac
Robert Drinovac <52541649+RobertD502@users.noreply.github.com>
2022-12-29 18:29:56 -0500

    import PasswordExpired exception

--- cowayaio/__init__.py
+++ cowayaio/__init__.py
@@ -2,10 +2,10 @@
 
 from .constants import (Endpoint, Endpoint_JSON, Header, Parameter, TIMEOUT,)
 from .coway_client import (CowayClient, LOGGER,)
-from .exceptions import (AuthError, CowayError,)
+from .exceptions import (AuthError, CowayError, PasswordExpired)
 from .purifier_model import (CowayPurifier, PurifierData,)
 from .str_enum import StrEnum
 
 __all__ = ['AuthError', 'CowayClient', 'CowayError', 'CowayPurifier',
-           'Endpoint', 'Endpoint_JSON', 'Header', 'LOGGER', 'Parameter',
-           'PurifierData', 'TIMEOUT', 'StrEnum']
+           'Endpoint', 'Endpoint_JSON', 'Header', 'LOGGER', 'PasswordExpired',
+           'Parameter', 'PurifierData', 'TIMEOUT', 'StrEnum']
