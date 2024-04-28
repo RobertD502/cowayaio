0311fa32e57f868ee4e43bb33a3c056b832a3f29
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2024-04-28 16:59:14 -0400

    add LightMode import

--- cowayaio/__init__.py
+++ cowayaio/__init__.py
@@ -1,11 +1,11 @@
 """Init file for CowayAIO"""
 
-from .constants import (Endpoint, Endpoint_JSON, Header, Parameter, TIMEOUT,)
+from .constants import (Endpoint, Endpoint_JSON, Header, LightMode, Parameter, TIMEOUT,)
 from .coway_client import (CowayClient, LOGGER,)
 from .exceptions import (AuthError, CowayError, PasswordExpired)
 from .purifier_model import (CowayPurifier, PurifierData,)
 from .str_enum import StrEnum
 
 __all__ = ['AuthError', 'CowayClient', 'CowayError', 'CowayPurifier',
-           'Endpoint', 'Endpoint_JSON', 'Header', 'LOGGER', 'PasswordExpired',
-           'Parameter', 'PurifierData', 'TIMEOUT', 'StrEnum']
+           'Endpoint', 'Endpoint_JSON', 'Header', 'LightMode', 'LOGGER',
+           'PasswordExpired', 'Parameter', 'PurifierData', 'TIMEOUT', 'StrEnum']
