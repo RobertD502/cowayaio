fa471992a3c1bba0a9551a3a28caa5993a49cac7
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2024-05-01 14:18:47 -0400

    bump to 0.1.10.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cowayaio",
-    version="0.1.10.2",
+    version="0.1.10.3",
     author="Robert Drinovac",
     author_email="unlisted@gmail.com",
     description="A asynchronous python library for Coway Air Purifiers ",
