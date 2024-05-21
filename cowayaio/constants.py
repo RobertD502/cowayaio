c1ef0853be0ef30af76534f2ff3706dc991ded7a
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2024-05-21 13:19:35 -0400

    add GET_TOKEN endpoint, use new redirect url, update client id, change GET_TOKEN trcode

--- cowayaio/constants.py
+++ cowayaio/constants.py
@@ -9,27 +9,30 @@ class Endpoint(StrEnum):
     CONTROL = '/control'
     DEVICE_LIST = '/com/user-devices'
     FILTERS = '/air/devices/'
+    GET_TOKEN = '/com/token'
     HOME = '/home'
     MCU_VERSION = '/com/ota'
     NEW_BASE_URI = 'https://iocareapi.iot.coway.com/api/v1'
     OAUTH_URL = "https://id.coway.com/auth/realms/cw-account/protocol/openid-connect/auth"
     PROD_SETTINGS = '/com/user-device-status'
-    REDIRECT_URL = "https://iocareapp.coway.com/bizmob.iocare/redirect/redirect_bridge.html"
+    REDIRECT_URL = "https://iocare-redirect.iot.coway.com/redirect_bridge.html"
     TOKEN_REFRESH = "https://iocareapi.iot.coway.com/api/v1/com/refresh-token"
-    
+
 
 class Parameter(StrEnum):
 
     APP_VERSION = "2.3.33"
-    CLIENT_ID = "cwid-prd-iocare-20220930"
+    CLIENT_ID = "cwid-prd-iocare-20240327"
     SERVICE_CODE = "com.coway.IOCareKor"
-    
+    CLIENT_NAME = "IOCARE"
+
 
 class Header(StrEnum):
 
     ACCEPT = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
     ACCEPT_LANG = "en-US,en;q=0.9"
     USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 17_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Version/10.0 Safari/602.1"
+    CONTENT_JSON = "application/json"
 
 
 class EndpointJSON(StrEnum):
@@ -38,12 +41,12 @@ class EndpointJSON(StrEnum):
     CONTROL = 'CWIG0603'
     DEVICE_LIST = 'CWIG0304'
     FILTERS = 'CWIA0120'
-    GET_TOKEN = 'CWIL0100'
+    GET_TOKEN = 'CWCC0009'
     MCU_VERSION = "CWIG0615"
     PROD_SETTINGS = "CWIG0301"
     STATUS = 'CWIG0602'
     TOKEN_REFRESH = 'CWCC0010'
-    
+
 
 class ErrorMessages(StrEnum):
 
