e3690d6dbcdccbcc123503d877121c8c2deb5516
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2025-10-15 12:27:05 -0400

    import NoPlaces and NoPurifiers exception

--- cowayaio/__init__.py
+++ cowayaio/__init__.py
@@ -2,12 +2,12 @@
 
 from .constants import (CATEGORY_NAME, Endpoint, ErrorMessages, Header, LightMode, Parameter, PREFILTER_CYCLE, TIMEOUT,)
 from .coway_client import (CowayClient, LOGGER,)
-from .exceptions import (AuthError, CowayError, PasswordExpired, RateLimited)
+from .exceptions import (AuthError, CowayError, NoPlaces, NoPurifiers, PasswordExpired, RateLimited)
 from .purifier_model import (CowayPurifier, PurifierData,)
 from .str_enum import StrEnum
 from .__version__ import __version__
 
 __all__ = ['AuthError', 'CATEGORY_NAME', 'CowayClient', 'CowayError', 'CowayPurifier',
-           'Endpoint', 'ErrorMessages', 'Header', 'LightMode', 'LOGGER',
+           'Endpoint', 'ErrorMessages', 'Header', 'LightMode', 'LOGGER', 'NoPlaces', 'NoPurifiers',
            'PasswordExpired', 'Parameter', 'PREFILTER_CYCLE', 'PurifierData',
            'RateLimited', 'StrEnum', 'TIMEOUT', '__version__']
